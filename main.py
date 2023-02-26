c556ae74f227b3bbd44c611f3335a9fea0f0adc8
Liza Firsova <lizafirsova2006@yandex.ru>
2023-02-26 20:37:36 +0500

    Main_Program

---
+++ main.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, redirect
+from data import db_session, jobs_api
+from data.users import User
+from data.jobs import Jobs
+from data.departments import Department
+from data.forms import RegisterForm, LoginForm, WorksForm, WorksRedactionForm, DepartmentsForm, DepartmentsRedactionForm
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Wrong email or password",
+                               form=form)
+    return render_template('login.html', title='Authorisation', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.confirm.data:
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="This user already exists")
+        user = User(
+            name=form.name.data,
+            email=form.login.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Registration', form=form)
+
+
+def main():
+    name_db = 'mars_explorer.db'
+    db_session.global_init(f"db/{name_db}")
+    app.register_blueprint(jobs_api.blueprint)
+    app.run(port=5000)
+
+
+if __name__ == '__main__':
+    main()
